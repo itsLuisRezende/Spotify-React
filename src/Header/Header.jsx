365c291ae4a8f879a7bebdbe195a751163d03e6e
itsLuisRezende <140192412+itsLuisRezende@users.noreply.github.com>
2024-01-26 16:03:16 -0300

    Header
    Desenvolvendo a primeira parte do meu projeto em react (Header).

---
+++ src/Header/Header.jsx
@@ -0,0 +1,36 @@
+import { } from "react"
+import "./Header.css"
+import arrowLeft from "../assets/icons/small-left.png"
+import arrowRight from "../assets/icons/small-right.png"
+
+function Header() {
+
+
+    return(
+        <>
+            <nav className="header__navigation">
+            <div className="navigation">
+                <button className="arrow-left">
+                    <img src={arrowLeft} alt="Seta esquerda"/>
+                </button>
+                <button className="arrow-right">
+                    <img src={arrowRight} alt="Seta direita"/>
+                </button>
+            </div>
+
+            <div className="header__search">
+                <img src="src/assets/icons/search.png" alt=""/>
+                <input id="search-input" type="text" maxlength="800" placeholder="O que você quer ouvir?" />
+            </div>
+
+            <div className="header__login">
+                <button className="subscribe">Inscreva-se</button>
+                <button className="login">Entrar</button>
+            </div>
+            
+            </nav> 
+        </>
+    )
+}
+
+export default Header
